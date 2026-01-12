9c7997c35ceac0ae890c9d7d0a144321838b7a8f
mac <mac@macdeMacBook-Pro.local>
2026-01-12 14:54:15 +0100

    chore: suppress next/no-img-element warning as we use unoptimized images

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,7 @@ export default [
       "react-hooks/set-state-in-effect": "off",
       "react-hooks/purity": "off",
       "import/no-anonymous-default-export": "off",
+      "@next/next/no-img-element": "off",
     },
   },
 ];
